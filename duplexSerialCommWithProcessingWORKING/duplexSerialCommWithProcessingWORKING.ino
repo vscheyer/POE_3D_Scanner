1c26d479a2cf5979f5d018918c0ca3bb99422176
rballaux <42899833+rballaux@users.noreply.github.com>
2018-12-12 13:35:29 -0500

    foking almost working lest go to sleep again
    whoooohoooo

--- duplexSerialCommWithProcessingWORKING/duplexSerialCommWithProcessingWORKING.ino
+++ duplexSerialCommWithProcessingWORKING/duplexSerialCommWithProcessingWORKING.ino
@@ -44,11 +44,21 @@ String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 int rowCounter = 0;
 
-int lockPos = 50;
-int unlockPos = 0;
-int stepperSize = 35;
+int lockPos = 70;
+int unlockPos = 120;
+int stepperSize = 37;
+float distanceArray[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,3,3,0,0,0,0},
+                              {0,0,0,3,3,3,3,0,0,0},
+                              {0,0,0,3,0,0,3,0,0,0},
+                              {0,0,3,0,0,0,0,3,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,3,3,0,0,0,0,3,3,0},
+                              {0,3,3,0,0,0,0,3,3,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0}};
 
-float distanceArray[10][10];
+//float distanceArray[10][10];
 int angleArray[10][10];
 
 void setup() {
@@ -58,12 +68,12 @@ void setup() {
   servo2.attach(25);
   servo3.attach(26);
   servo4.attach(27);
-  servo1.attach(28);
-  servo2.attach(29);
-  servo3.attach(30);
-  servo4.attach(31);
-  servo3.attach(32);
-  servo4.attach(33);
+  servo5.attach(28);
+  servo6.attach(29);
+  servo7.attach(30);
+  servo8.attach(31);
+  servo9.attach(32);
+  servo10.attach(33);
   servo1.write(0);
   servo2.write(0);
   servo3.write(0);
@@ -87,14 +97,14 @@ void setup() {
   lockServo10.attach(43);
   lockServo1.write(unlockPos);
   lockServo2.write(unlockPos);
-  lockServo3.write(unlockPos);
-  lockServo4.write(unlockPos);
-  lockServo5.write(unlockPos);
-  lockServo6.write(unlockPos);
-  lockServo7.write(unlockPos);
-  lockServo8.write(unlockPos);
-  lockServo9.write(unlockPos);
-  lockServo10.write(unlockPos);
+  lockServo3.write(unlockPos - 20);
+  lockServo4.write(unlockPos - 20);
+  lockServo5.write(unlockPos + 20);
+  lockServo6.write(unlockPos + 20);
+  lockServo7.write(unlockPos - 20);
+  lockServo8.write(unlockPos - 30);
+  lockServo9.write(unlockPos - 30);
+  lockServo10.write(unlockPos - 30);
 
   stepper1.connectToPins(stp, dir);
   pinMode(MS1, OUTPUT);
@@ -109,8 +119,11 @@ void setup() {
   stepper1.setStepsPerMillimeter(40);
 
   //Set speed and acceleration in mm per second
-  stepper1.setSpeedInMillimetersPerSecond(10);
+  stepper1.setSpeedInMillimetersPerSecond(20);
   stepper1.setAccelerationInMillimetersPerSecondPerSecond(10);
+  delay(5000);
+  distanceToAngle();
+  delay(10000);
 
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
@@ -155,70 +168,116 @@ void getInput() {
     distanceArray[rowCounter][c] = inputString.substring(inputString.indexOf(',') + 1, inputString.indexOf(",", +1)).toFloat(); // find string between 2 commas
     inputString.remove(0, 5); //remove first comma and number
   }
+  //    Serial.print(distanceArray[rowCounter][rowCounter]);
 }
 void writeToServos() {
-  for (int r = 0; r < 4; r++) {
+  for (int r = 0; r < 10; r++) {
+    //    for (int i = 0; i < angleArray[r][0]; i++) {
     servo1.write(angleArray[r][0]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][1]; i++) {
     servo2.write(angleArray[r][1]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][2]; i++) {
     servo3.write(angleArray[r][2]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][3]; i++) {
     servo4.write(angleArray[r][3]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][4];i++) {
     servo5.write(angleArray[r][4]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][5]; i++) {
     servo6.write(angleArray[r][5]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][6]; i++) {
     servo7.write(angleArray[r][6]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][7]; i++) {
     servo8.write(angleArray[r][7]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][8]; i++) {
     servo9.write(angleArray[r][8]);
+    delay(15);
+    //    }
+    //    for (int i = 0; i < angleArray[r][9]; i++) {
     servo10.write(angleArray[r][9]);
+    delay(15);
+    //    }
+    //    servo2.write(angleArray[r][1]);
+    //    servo3.write(angleArray[r][2]);
+    //    servo4.write(angleArray[r][3]);
+    //    servo5.write(angleArray[r][4]);
+    //    servo6.write(angleArray[r][5]);
+    //    servo7.write(angleArray[r][6]);
+    //    servo8.write(angleArray[r][7]);
+    //    servo9.write(angleArray[r][8]);
+    //    servo10.write(angleArray[r][9]);
     delay(1000);
 
     if (r == 0) {
       lockServo1.write(lockPos);
-      stepper1.moveToPositionInMillimeters(0);
     }
-    if (r == 1) {
-      lockServo2.write(lockPos);
-      stepper1.moveToPositionInMillimeters(1*stepperSize);
+    else if (r == 1) {
+      lockServo2.write(lockPos-10);
     }
-    if (r == 2) {
-      lockServo3.write(lockPos);
-      stepper1.moveToPositionInMillimeters(2*stepperSize);
+    else if (r == 2) {
+      lockServo3.write(lockPos - 20);
     }
-    if (r == 3) {
-      lockServo4.write(lockPos);
-      stepper1.moveToPositionInMillimeters(3*stepperSize);
+    else if (r == 3) {
+      lockServo4.write(lockPos - 20);
     }
-    if (r == 4) {
-      lockServo5.write(lockPos);
-      stepper1.moveToPositionInMillimeters(4*stepperSize);
+    else if (r == 4) {
+      lockServo5.write(lockPos + 20);
     }
-    if (r == 5) {
-      lockServo6.write(lockPos);
-      stepper1.moveToPositionInMillimeters(5*stepperSize);
+    else if (r == 5) {
+      lockServo6.write(lockPos + 20);
     }
-    if (r == 6) {
-      lockServo7.write(lockPos);
-      stepper1.moveToPositionInMillimeters(6*stepperSize);
+    else if (r == 6) {
+      lockServo7.write(lockPos - 20);
     }
-    if (r == 7) {
-      lockServo8.write(lockPos);
-      stepper1.moveToPositionInMillimeters(7*stepperSize);
+    else if (r == 7) {
+      lockServo8.write(lockPos - 30);
     }
-    if (r == 8) {
-      lockServo9.write(lockPos);
-      stepper1.moveToPositionInMillimeters(8*stepperSize);
+    else if (r == 8) {
+      lockServo9.write(lockPos - 30);
     }
-    if (r == 9) {
-      lockServo10.write(lockPos);
-      stepper1.moveToPositionInMillimeters(9*stepperSize);
+    else if (r == 9) {
+      lockServo10.write(lockPos - 30);
     }
-    delay(200);
+    delay(500);
     resetPushBlocks();
-    delay(3000);
+    if (r < 9) {
+      stepper1.moveToPositionInMillimeters(-(r + 1) * stepperSize);
+    }
+    else {
+      stepper1.moveToPositionInMillimeters(0);
+      lockServo1.write(unlockPos);
+      lockServo2.write(unlockPos);
+      lockServo3.write(unlockPos - 20);
+      lockServo4.write(unlockPos - 20);
+      lockServo5.write(unlockPos + 20);
+      lockServo6.write(unlockPos + 20);
+      lockServo7.write(unlockPos - 20);
+      lockServo8.write(unlockPos - 30);
+      lockServo9.write(unlockPos - 30);
+      lockServo10.write(unlockPos - 30);
+      delay(10000);
+    }
   }
 }
 void distanceToAngle() {
   for (int c = 0; c < 10; c++) {
     for (int r = 0; r < 10; r++) {
-      angleArray[r][c] = ourMap(distanceArray[r][c], 0, 4, 180, 0);
+      angleArray[r][c] = ourMap(distanceArray[r][c], 0,4, 0, 160);
     }
   }
   writeToServos();
@@ -235,6 +294,6 @@ void resetPushBlocks() {
   servo9.write(0);
   servo10.write(0);
 }
-int ourMap(float value, long in_min, long in_max, long out_min, long out_max) {
-  return (int)(value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+int ourMap(float value, float in_min, float in_max, float out_min, float out_max) {
+  return (int)(((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min);
 }
